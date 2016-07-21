d17b4622b0cf2aff745e8c4e924b479deb0e9719
r-frederick <ridge.frederick@gmail.com>
2016-07-20 20:06:26 -0500

    use slack api methods and make text unicode friendly

--- notifier.py
+++ notifier.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import json
 from slackclient import SlackClient
 from geopy.geocoders import Nominatim
@@ -36,9 +38,8 @@ def pokemon_found(pokemon):
     print "[+] Notifier found pokemon:", pokename
     gMaps = "http://maps.google.com/maps?q=" + str(pokemon["lat"]) + "," + str(pokemon["lng"]) + "&24z"
     disappear_time = str(datetime.fromtimestamp(pokemon["disappear_time"]).strftime("%I:%M%p").lstrip('0'))
-    notification_text = 'A wild {0} was found nearby and will disappear at {1}! {2}'.format(pokemon["name"], disappear_time, gMaps)
-    slack_client.rtm_connect()
-    slack_client.rtm_send_message("#pokedev", notification_text)
+    notification_text = u'A wild {0} was found nearby and will disappear at {1}! {2}'.format(pokemon["name"], disappear_time, gMaps)
+    slack_client.api_call("chat.postMessage", channel="#pokedev", as_user=True, text=notification_text)
 
 
 init()
