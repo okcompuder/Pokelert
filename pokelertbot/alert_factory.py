7978195a32a3e79e4047db1abdd853861cb4de31
okcompuder <dgrujo@gmail.com>
2016-07-29 01:53:55 -0500

    set attachment color based on type

--- pokelertbot/alert_factory.py
+++ pokelertbot/alert_factory.py
@@ -10,23 +10,27 @@ class AlertFactory(object):
         return alert
 
     def nearby_alert(self, pokemon):
-        text = u'Wild *{}* appeared!'.format(pokemon['pokemon_name'])
+        text = ''
         attachments = self.nearby_attachments(pokemon)
         return { 'text': text, 'attachments': attachments}
 
     def nearby_attachments(self, pokemon):
         id = str(pokemon['pokemon_id']).zfill(3)
         url = 'https://fevgames.net/pokedex/{}-{}/'.format(id, pokemon['pokemon_name'])
+        author_name = u'Wild {} appeared!'.format(pokemon['pokemon_name'])
+        color = self.format_color(pokemon)
+        
         attachments = \
         [{
             'fallback':'Error',
-            'color':'#439FE0',
-            'author_name': 'Scan',
+            'color': color,
+            'title': author_name,
             'author_link': url,
             'fields': self.nearby_fields(pokemon),
             'thumb_url':'http://pogo.ethanhoneycutt.com/static/larger-icons/{}.png'.format(pokemon['pokemon_id']),
             'footer':'<{}|Github>'.format('https://github.com/okcompuder/Pokelert'),
-            'footer_icon':'http://androidforum.cz/images/icons/hw/ext/pokemon_go.png'
+            'footer_icon':'http://androidforum.cz/images/icons/hw/ext/pokemon_go.png',
+            'mrkdwn_in': ['title']
         }]
         return attachments
 
@@ -45,6 +49,19 @@ class AlertFactory(object):
                  ]
         return fields
 
+    def format_color(self, pokemon):
+        first_type = pokemon['pokedex_info']['Types'][0]
+        return self.color_map(first_type)   
+        
+    def color_map(self, type):
+        colors = {
+            'Normal': '#000000',
+            'Grass': '#228B22',
+            'Bug': '#00FF00',
+            'Water': '#1E90FF'
+        }
+        return colors.get(type, '#FFFFFF')
+        
     def format_distance(self, lat, long):
         return '400 Feet NW'
 
